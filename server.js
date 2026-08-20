f6173b9c5f00b5ad5f50c3ab49cb9defef65cfe5
buzzyt <ar.unitedspb@gmail.com>
2026-08-20 16:39:33 +0100

    Replace viralbell.netlify.app with bagzt.netlify.app

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://buzzyt.netlify.app',
   'https://bellbusinesscanada.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://viralbell.netlify.app',
+  'https://bagzt.netlify.app',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
