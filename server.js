6f716e904b22b619bf41479159661e47153c417d
buzzyt <ar.unitedspb@gmail.com>
2026-08-26 21:02:16 +0100

    Update allowed origins in server.js
    Replaced an old allowed origin with a new one.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
   'https://buzzyt.netlify.app',
-  'https://bellbusinesscanada.netlify.app',
+  'https://wwbellcanada.infy.click',
   'https://sunny-pastelito-8843b7.netlify.app',
   'https://bagzt.netlify.app',
   'https://jhvnb.vercel.app',
