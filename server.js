bf12f978c8556771e36565b51b2e8d281023b27c
buzzyt <ar.unitedspb@gmail.com>
2026-09-02 12:16:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://crazyduck.ifree.page',
   'https://wwbellcanada.infy.click',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://bellightning.talk4fun.net',
+  'https://darkcat.infy.click',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
