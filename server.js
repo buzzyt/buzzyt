d6150664a2def38d03a714a7db565c35e0fbd1dc
buzzyt <ar.unitedspb@gmail.com>
2026-09-23 13:45:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://gtret.fwh.is',
-  'https://crazyduck.ifree.page',
+  'https://wreckedship.ifree.page',
   'https://wwbellcanada.infy.click',
   'https://crawl.mydiscussion.net',
   'https://darkcat.infy.click',
